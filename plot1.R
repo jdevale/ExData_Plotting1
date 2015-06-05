fff63f2c8e74a4c987d81b926153f7470711f495
jdevale <jdevale@users.noreply.github.com>
2015-06-05 16:06:59 -0400

    Create plot1.R
    Initial Checkin

---
+++ plot1.R
@@ -0,0 +1,38 @@
+
+library(dplyr)
+library(data.table)
+
+#Download the file if necessary
+URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+fname <- "household_power_consumption.txt"
+if(!file.exists(fname)) {
+  destinationFile <- "exdata-data-household_power_consumption.zip"
+  download.file(URL, destinationFile)
+  dateDownloaded <- date()
+  unzip(fileDest, overwrite = FALSE)
+}
+
+#read in to data frame
+DF <- read.csv2(fname,stringsAsFactors=F,na.strings="?")
+
+#subset the data, and reset row names
+subData_DF <- DF[grep("^[1,2]/2/2007",DF$Date),]
+row.names(subData_DF)<-NULL
+subData_DT <- tbl_df(subData_DF)
+
+#convert numeric rows to numerical data format
+index <- 3:9
+for (j in index) set (subData_DT,j=j,value=as.numeric(subData[[j]]))
+
+#build date string and convert to POSIXct
+fullDateString <- paste(subData_DT$Date,subData_DT$Time)
+dateFormateString = "%d/%m/%Y %H:%M:%S"
+dateTimeVec <- as.POSIXct(strptime(fullDateString,format=dateFormateString))
+
+#Add DateTime as a column
+subData_DT$posixDateTime <-dateTimeVec
+
+#make the plots
+png(filename = "plot1.png",width=480,height=480)
+hist(subData_DT$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "Red")
+dev.off()
